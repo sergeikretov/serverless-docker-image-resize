642832ee7e9d8cc27be1dde577a85808753ab400
Sergei Kretov <serge.k.a@gmail.com>
2019-02-01 04:00:25 +0200

    add requested image size validation add MAX size -> 8k

--- functions/resize.js
+++ functions/resize.js
@@ -1,77 +1,103 @@
-const stream = require('stream')
-const AWS = require('aws-sdk')
+const stream = require('stream');
+const AWS = require('aws-sdk');
 const S3 = new AWS.S3({
-  signatureVersion: 'v4'
-})
-const sharp = require('sharp')
-const BUCKET = process.env.BUCKET
-const URL = `http://${process.env.BUCKET}.s3-website.${process.env.REGION}.amazonaws.com`
+    signatureVersion: 'v4'
+});
+const sharp = require('sharp');
+const BUCKET = process.env.BUCKET;
+const URL = `http://${process.env.BUCKET}.s3-website.${process.env.REGION}.amazonaws.com`;
+
+// 8K
+const MAX_WIDTH  = 7680;
+const MAX_HEIGHT = 7680;
 
 // create the read stream abstraction for downloading data from S3
-const readStreamFromS3 = ({ Bucket, Key }) => {
-  return S3.getObject({ Bucket, Key }).createReadStream()
-}
+const readStreamFromS3 = ({Bucket, Key}) => {
+    return S3.getObject({Bucket, Key}).createReadStream()
+};
 // create the write stream abstraction for uploading data to S3
-const writeStreamToS3 = ({ Bucket, Key }) => {
-  const pass = new stream.PassThrough()
-  return {
-    writeStream: pass,
-    uploadFinished: S3.upload({
-      Body: pass,
-      Bucket,
-      ContentType: 'image/png',
-      Key
-    }).promise()
-  }
-}
+const writeStreamToS3 = ({Bucket, Key}) => {
+    const pass = new stream.PassThrough();
+    return {
+        writeStream: pass,
+        uploadFinished: S3.upload({
+            Body: pass,
+            Bucket,
+            ContentType: 'image/png',
+            Key
+        }).promise()
+    }
+};
 // sharp resize stream
-const streamToSharp = ({ width, height }) => {
-  return sharp()
-    .resize(width, height)
-    .toFormat('png')
-}
+const streamToSharp = ({width, height}) => {
+    return sharp()
+        .resize(width, height)
+        .toFormat('png')
+};
 
 exports.handler = async (event) => {
-  const key = event.queryStringParameters.key
-  const match = key.match(/(\d+)x(\d+)\/(.*)/)
-  const width = parseInt(match[1], 10)
-  const height = parseInt(match[2], 10)
-  const originalKey = match[3]
-  const newKey = '' + width + 'x' + height + '/' + originalKey
-  const imageLocation = `${URL}/${newKey}`
+    const key = event.queryStringParameters.key;
+    const match = key.match(/(\d+)x(\d+)\/(.*)/);
 
-  try {
-    // create the read and write streams from and to S3 and the Sharp resize stream
-    const readStream = readStreamFromS3({ Bucket: BUCKET, Key: originalKey })
-    const resizeStream = streamToSharp({ width, height })
-    const { writeStream, uploadFinished } = writeStreamToS3({ Bucket: BUCKET, Key: newKey })
-
-    // trigger the stream
-    readStream
-      .pipe(resizeStream)
-      .pipe(writeStream)
-
-    // wait for the stream to finish
-    const uploadedData = await uploadFinished
-
-    // log data to Dashbird
-    console.log('Data: ', {
-      ...uploadedData,
-      BucketEndpoint: URL,
-      ImageURL: imageLocation
-    })
-
-    // return a 301 redirect to the newly created resource in S3
-    return {
-      statusCode: '301',
-      headers: { 'location': imageLocation },
-      body: ''
+    if (!match || !match[1] || !match[2] || !match[3]) {
+        return {
+            statusCode: '500',
+            body: `invalid request format`
+        }
     }
-  } catch (err) {
-    console.error(err)
-    return {
-      statusCode: '500',
-      body: err.message
+
+    const width = parseInt(match[1], 10);
+    const height = parseInt(match[2], 10);
+    const originalKey = match[3];
+
+
+    if (isNaN(width) || isNaN(height)) {
+        return {
+            statusCode: '500',
+            body: `width and height parameters must be integer`
+        }
+    } else if (width > MAX_WIDTH || height > MAX_HEIGHT) {
+        return {
+            statusCode: '500',
+            body: `Image size is too big. Can not exceed 8k resolution.`
+        }
     }
-  }
-}
+
+    const newKey = '' + width + 'x' + height + '/' + originalKey;
+    const imageLocation = `${URL}/${newKey}`;
+
+    try {
+        // create the read and write streams from and to S3 and the Sharp resize stream
+        const readStream = readStreamFromS3({Bucket: BUCKET, Key: originalKey});
+        const resizeStream = streamToSharp({width, height});
+        const {writeStream, uploadFinished} = writeStreamToS3({Bucket: BUCKET, Key: newKey});
+
+        // trigger the stream
+        readStream
+            .pipe(resizeStream)
+            .pipe(writeStream);
+
+        // wait for the stream to finish
+        const uploadedData = await uploadFinished;
+
+        // log data to Dashbird
+        console.log('Data: ', {
+            ...uploadedData,
+            BucketEndpoint: URL,
+            ImageURL: imageLocation
+        });
+
+        // return a 301 redirect to the newly created resource in S3
+        return {
+            statusCode: '301',
+            headers: {'location': imageLocation},
+            body: ''
+        }
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: '500',
+            body: err.message
+        }
+    }
+};
